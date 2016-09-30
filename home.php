98f963a6ef180412da94baead4947b11e0aac1dd
harrisonhornsby <hhorn001@odu.edu>
2016-09-30 19:23:48 +0000

    Add files

---
+++ home.php
@@ -0,0 +1,123 @@
+<?php
+
+	require_once("session.php");
+	
+	require_once("class.user.php");
+	$auth_user = new USER();
+	
+	
+	$user_id = $_SESSION['user_session'];
+	
+	$stmt = $auth_user->runQuery("SELECT * FROM users WHERE user_id=:user_id");
+	$stmt->execute(array(":user_id"=>$user_id));
+	
+	$userRow=$stmt->fetch(PDO::FETCH_ASSOC);
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+<link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet" media="screen">
+<script type="text/javascript" src="jquery-1.11.3-jquery.min.js"></script>
+<link rel="stylesheet" href="style.css" type="text/css"  />
+<title>welcome - <?php print($userRow['user_email']); ?></title>
+</head>
+
+<body>
+
+<nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="http://localhost">SlackOverflow</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="http://www.google.com">Some Tab Link</li>
+            <li><a href="http://www.google.com">Some Lower Level Tab Link</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+			  <span class="glyphicon glyphicon-user"></span>&nbsp;Hello <?php echo $userRow['user_email']; ?>&nbsp;<span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="profile.php"><span class="glyphicon glyphicon-user"></span>&nbsp;View Profile</a></li>
+                <li><a href="logout.php?logout=true"><span class="glyphicon glyphicon-log-out"></span>&nbsp;Sign Out</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+
+    <div class="clearfix"></div>
+    	
+    
+<div class="container-fluid" style="margin-top:80px;">
+	
+    <div class="container">
+    
+    	<label class="h5">welcome : <?php print($userRow['user_name']); ?></label>
+        <hr />
+        
+        <h1>
+        <a href="home.php"><span class="glyphicon glyphicon-home"></span> home</a> &nbsp; 
+        <a href="profile.php"><span class="glyphicon glyphicon-user"></span> profile</a></h1>
+       	<hr />
+        
+        <p class="h4">User Home Page</p>
+
+        <!-- Table that will display questions -->
+        <p>PHP STUFF BELOW</p>
+        <?php
+        $servername = "localhost";
+        $username = "root";
+        $password = "odu2017";
+        $dbname = "slackoverflow";
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        $sql = "SELECT question, question_id, asker_id, answer_id FROM questions";
+                $result = $conn->query($sql);
+
+        echo "<table id=\"questionTable\"> 
+                <th>Question</th>
+                <th>Question ID</th>
+                <th>Asker ID</th>
+                <th>Answer ID</th>";
+        if($result->num_rows > 0)
+        {
+            while($row = $result->fetch_assoc())
+            {
+                echo "<tr>
+                    <td>".$row["question"] ."</td>
+                    <td>".$row["question_id"] ."</td>
+                    <td>".$row["asker_id"] ."</td>
+                    <td>".$rowUser["user_name"] ."</td>
+                    <td>".$row["answer_id"] ."</td>
+                    </tr>";
+            }
+        }
+        echo "</table>";
+
+
+        $conn->close();
+         ?>
+
+
+
+    </div>
+
+</div>
+
+<script src="bootstrap/js/bootstrap.min.js"></script>
+
+</body>
+</html>
