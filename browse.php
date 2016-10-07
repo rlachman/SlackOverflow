da2a914bfcedd8182e8394cc6f5b9f0ddb9e097f
harrisonhornsby <hhorn001@odu.edu>
2016-10-07 01:33:05 +0000

    Tweaked style sheet

---
+++ browse.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<head></head>
+<title></title>
+<body>
+<h1>Not yet implemented. Will be capable of browsing past question/answers and more!</h1>
+<h1><a href=home.php> Home </a></h1>
+</body>
+</html>
