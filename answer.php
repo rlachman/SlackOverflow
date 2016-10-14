01683e4e048c51388f87e2fe5e47210548115c53
harrisonhornsby <hhorn001@odu.edu>
2016-10-14 18:05:42 +0000

    Fixed issue with guest validation allowing guest to post answer.

--- answer.php
+++ answer.php
@@ -1,29 +1,25 @@
 <?php
-        session_start();
-        require_once("session.php");
-        require_once("class.user.php");
-        $auth_user = new USER();
-        
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $dbname = "slackoverflow";
-        
-        $conn = new mysqli($servername, $username, $password, $dbname);
-		
-		if ($conn->connect_error) {
-    	die("Connection failed: " . $conn->connect_error);
-		} 
-		if(!$conn->connect_error)
-		{
-			echo "Connection OKAY.";
-		}
-
+ require_once("session.php");
+  
+  require_once("class.user.php");
+  $auth_user = new USER();
+  
+  
+  $user_id = $_SESSION['user_session'];
+  
+  $servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "slackoverflow";
+$conn = new mysqli($servername, $username, $password, $dbname);
   $stmt = $auth_user->runQuery("SELECT * FROM users WHERE user_id=:user_id");
   $stmt->execute(array(":user_id"=>$user_id));
   
   $userRow=$stmt->fetch(PDO::FETCH_ASSOC);
-  $_SESSIONS['user_name'] = $userRow['user_name'];
+
+  $_SESSION['user_id'] = $userRow['user_id'];
+  $_SESSION['user_name'] = $userRow['user_name'];
+  $user_is_guest = $userRow['is_guest'];
 
 
 		$q_id = $_GET["q_id"];
@@ -49,33 +45,65 @@
 </head>
 
 <nav class="navbar navbar-default navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="logo" href="home.php">SlackOverflow</a>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-          </ul>
-          <ul class="nav navbar-nav navbar-right">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="logo" href="home.php">SlackOverflow</a>
+    </div>
+    <div id="navbar" class="navbar-collapse collapse">
+      <ul class="nav navbar-nav">
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+
+        <li class="dropdown">
+          <a href="#" id="dropdown" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
             
-            <li class="dropdown">
-              <a href="#" id="dropdown" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-			  <span class="glyphicon glyphicon-user"></span>&nbsp;Hello <?php echo $userRow['user_email']; ?>&nbsp;<span class="caret"></span></a>
-              <ul class="dropdown-menu">
-                <li><a href="profile.php"><span class="glyphicon glyphicon-user"></span>&nbsp;View Profile</a></li>
-                <li><a href="logout.php?logout=true"><span class="glyphicon glyphicon-log-out"></span>&nbsp;Sign Out</a></li>
-              </ul>
-            </li>
+            <span class="glyphicon glyphicon-user"></span>&nbsp;Hello 
+              <?php 
+                  if($user_is_guest == FALSE) 
+                  {
+                    echo $userRow['user_email'];
+                  }
+                  else{
+                    echo " Guest";
+                  }
+
+              ?>&nbsp;
+              <span class="caret">
+              </span>
+          </a>
+          
+          <ul class="dropdown-menu">
+            <?php 
+                  if($user_is_guest == FALSE) 
+                  {
+                    echo "<li><a href=\"profile.php\"><span class=\"glyphicon glyphicon-user\"></span>&nbsp;View Profile</a></li>";
+                  }
+            
+            if(!$auth_user->is_loggedin() or $user_is_guest = TRUE)
+            {
+              echo "<li><a href=\"logout.php?logout=true\"><span class=\"glyphicon glyphicon-off\"></span>&nbsp;Sign In</a></li>";
+            }
+
+            if($auth_user->is_loggedin() and $user_name != "guest" and $user_is_guest != TRUE)
+            {
+              echo "<li><a href=\"logout.php?logout=true\"><span class=\"glyphicon glyphicon-log-out\"></span>&nbsp;Sign Out</a></li>";
+            }
+            
+
+
+            ?>
           </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </nav>
+        </li>
+      </ul>
+    </div><!--/.nav-collapse -->
+  </div>
+</nav>
 
     <div class="clearfix"></div>
     	
