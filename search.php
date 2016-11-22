0b125058652b4b2d197123106a86ff818af19f37
harrisonhornsby <hhorn001@odu.edu>
2016-11-22 04:51:23 +0000

    ajax search added

--- search.php
+++ search.php
@@ -128,33 +128,44 @@ function Solved($solved)
           <?php if($_SESSION['isAdmin']) echo '<a href="admin.php"><span class="glyphicon glyphicon-cog"></span> AdminCP</a>'; ?>
           </h1>
         
-        <H1>FAQ</H1>
-          <HR>
-
-          <B>What is SlackOverflow?</B><BR>
-          <BLOCKQUOTE>SlackOverflow is a question and answer forum.</BLOCKQUOTE>
-
-          <B>How do I ask a question?</B><BR>
-          <BLOCKQUOTE>You must <a href="sign-up.php"> Sign Up</a> to SlackOverflow to ask or answer any questions.</BLOCKQUOTE>
-
-          <B>How do I sign up?</B><BR>
-          <BLOCKQUOTE>To sign up you must click the Guest drop down menu on the top right of the page and choose <a href="index.php?home=yes"> Sign In</a>. Once at the <a href="index.php?home=yes"> Sign In</a> window, click <a href="sign-up.php"> Sign Up</a> at the bottom.</BLOCKQUOTE>
-
-          <B>What information is needed to sign up?</B><BR>
-          <BLOCKQUOTE>To sign up simply enter a Username, Email, and Password on the <a href="sign-up.php"> Sign Up</a> page.</BLOCKQUOTE>
-
-          <B>All signed up, but how do I ask a question?</B><BR>
-          <BLOCKQUOTE>In order to ask a question a member can click <a href="ask.php"> Ask</a> at the top of the page.</BLOCKQUOTE>
-
-          <B>How do I add my profile picture?</B><BR>
-          <BLOCKQUOTE>To add a profile image navigate to the profile page via <a href="profile.php"> Profile</a> on the top of the page. Once there, click Choose File. Select an image then click Submit. </BLOCKQUOTE>
-
-          <B>How do I see past questions?</B><BR>
-          <BLOCKQUOTE>To see any old question navigate to the browse page by clicking <a href="browse.php"> Browse</a> at the top of the page. As a side note if looking for your past asked questions simply navigate to your <a href="profile.php"> Profile</a>.</BLOCKQUOTE>
-
-          <B>What are these arrows next to the questions and answers?</B><BR>
-          <BLOCKQUOTE>Next to each question is an Up or Down arrow. These arrows allow members to vote a question up or down. The higher they are voted up the more they move to the top of the questions/answers list. Vice versa for voting down.</BLOCKQUOTE>
-
+        <div class="container">  
+                <br />  
+                <h2 align="center">Search For a User</h2><br />  
+                <div class="form-group">  
+                     <div class="input-group">  
+                          <span class="input-group-addon">Search</span>  
+                          <input type="text" name="search_text" id="search_text" placeholder="Enter A Username" class="form-control" />  
+                     </div>  
+                </div>  
+                <br />  
+                <div id="result"></div>  
+           </div>  
+      </body>  
+ </html>  
+ <script>  
+ $(document).ready(function(){  
+      $('#search_text').keyup(function(){  
+           var txt = $(this).val();  
+           if(txt != '')  
+           {  
+                $.ajax({  
+                     url:"searchDatabase.php",  
+                     method:"post",  
+                     data:{search:txt},  
+                     dataType:"text",  
+                     success:function(data)  
+                     {  
+                          $('#result').html(data);  
+                     }  
+                });  
+           }  
+           else  
+           {  
+                $('#result').html('');                 
+           }  
+      });  
+ });  
+ </script> 
 
     </div>
 
